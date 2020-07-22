9c2399893c645dde15ae0ac9d9f773c7e29cba73
Chris Paine <cdpaine2003@gmail.com>
2020-07-22 15:09:38 -0400

    Fist pull request new index.html, script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randStartBtn = document.getElementById("randstart");
+var randEndBtn = document.getElementById("randend");
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function randomColorHex() {
+	return Math.floor(Math.random()*16777215).toString(16);
+}
+
+//On page load
+body.style.background = "linear-gradient(to right, " 
++ "#00ff00" + ", " + "#ff0000" 	+ ")";
+
+css.textContent = body.style.background + ";";
+
+//Events
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+randStartBtn.addEventListener("click", function() {
+	var hex = randomColorHex();
+	color1.value = "#" + hex;
+	setGradient();
+});
+randEndBtn.addEventListener("click", function() {
+	var hex = randomColorHex();
+	color2.value = "#" + hex;
+	setGradient();
+});
